833c4d32f8671efd408c3f7c1209a838ece843c1
Rothenhouser <n.rothenhaeusler@gmail.com>
2014-11-18 15:17:26 +0100

    Fix

--- ex_2.m
+++ ex_2.m
@@ -15,8 +15,8 @@ wank_ys_raw = importdata('WANK_SATY');
 wank_zs_raw = importdata('WANK_SATZ');
 epochs = importdata('Epochs.txt');
 epochs = epochs(:,1);
-% 3 (a): Correct satellite positions for Earth rotation during light
-% travel time.
+%% 3 (a): Correct satellite positions for Earth rotation during light
+%% travel time.
 % Raw geometric distance:
 rho_sr_raw = sqrt((wank_xs_raw - wank_xr).^2 ...
 + (wank_ys_raw - wank_yr).^2 + (wank_zs_raw - wank_zr).^2);
@@ -29,7 +29,7 @@ wank_zs = wank_zs_raw;
 % Recompute the geometric distance.
 rho_sr = sqrt((wank_xs - wank_xr).^2 ...
 + (wank_ys - wank_yr).^2 + (wank_zs - wank_zr).^2);
-% 3 (b) Compute the derivates of the observation equation.
+%% 3 (b) Compute the derivates of the observation equation.
 zeros= [0 0 0 0 0 0 0];
 dPdt = 1;
 cs =[1 1 1 1 1 1 1] * dPdt;
@@ -38,22 +38,25 @@ dPdy = -(wank_ys - wank_yr) ./ rho_sr;
 dPdz = -(wank_zs - wank_zr) ./ rho_sr;
 % Correct pseudorange data with satellite clock correction.
 c1_corrected = wank_c1 + (c * wank_satt); % changed sign before bracket
+%% 
 for i=1:length(epochs);
-% Setup the grand design matrix A for every epoch.
-A(:,1)=dPdx(i,:);
-A(:,2)=dPdy(i,:);
-A(:,3)=dPdz(i,:);
-A(:,4)=cs;
-% Solve normal equation.
-deltay(i,:) = c1_corrected(i,:) - rho_sr(i,:);
-atransp = transpose(A);
-N = atransp*A;
-deltap(:,i) = (N \ atransp) * transpose(deltay(i,:));
-% Compute the residuals.
-epsilon(:,i) = transpose(deltay(i,:)) - (A * deltap(:,i));
-m0(:,i) = sqrt(transpose(epsilon(:,i)) * epsilon(:,i) ./ 3);
-sigmax(:,i) = m0(:,i) * Q(1,1);
-sigmay(:,i) = m0(:,i) * Q(2,2);
-sigmaz(:,i) = m0(:,i) * Q(3,3);
-sigmat(:,i) = m0(:,i) * Q(4,4);
+    % Setup the grand design matrix A for every epoch.
+    A(:,1)=dPdx(i,:);
+    A(:,2)=dPdy(i,:);
+    A(:,3)=dPdz(i,:);
+    A(:,4)=cs;
+    % Solve normal equation.
+    deltay(i,:) = c1_corrected(i,:) - rho_sr(i,:);
+    atransp = transpose(A);
+    N = atransp*A;
+    deltap(:,i) = (N \ atransp) * transpose(deltay(i,:));
+    % Compute the residuals.
+    epsilon(:,i) = transpose(deltay(i,:)) - (A * deltap(:,i));
+    m0(:,i) = sqrt(transpose(epsilon(:,i)) * epsilon(:,i) ./ 3);
+    % Formal errors
+    Q = inv(N);
+    sigmax(:,i) = m0(:,i) * Q(1,1);
+    sigmay(:,i) = m0(:,i) * Q(2,2);
+    sigmaz(:,i) = m0(:,i) * Q(3,3);
+    sigmat(:,i) = m0(:,i) * Q(4,4);
 end
