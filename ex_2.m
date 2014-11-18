b1aa879c7ff2831925539fc2f3a4331bf9a1182c
Rothenhouser <n.rothenhaeusler@gmail.com>
2014-11-18 17:09:19 +0100

    Add troposphere correction code.

--- ex_2.m
+++ ex_2.m
@@ -29,7 +29,7 @@ wank_zs = wank_zs_raw;
 rho_sr = sqrt((wank_xs - wank_xr).^2 ...
 + (wank_ys - wank_yr).^2 + (wank_zs - wank_zr).^2);
 %% 3 (b) Compute the derivates of the observation equation.
-zeros= [0 0 0 0 0 0 0];
+% Todo: change this back to c? Need to adjust calculations below!
 dPdt = 1;
 cs =[1 1 1 1 1 1 1] * dPdt;
 dPdx = -(wank_xs - wank_xr) ./ rho_sr;
@@ -37,7 +37,7 @@ dPdy = -(wank_ys - wank_yr) ./ rho_sr;
 dPdz = -(wank_zs - wank_zr) ./ rho_sr;
 % Correct pseudorange data with satellite clock correction.
 c1_corrected = wank_c1 + (c * wank_satt); % changed sign before bracket
-%% 
+%% Solve the normal equations.
 for i=1:length(epochs);
     % Setup the grand design matrix A for every epoch.
     A(:,1)=dPdx(i,:);
@@ -48,6 +48,7 @@ for i=1:length(epochs);
     deltay(i,:) = c1_corrected(i,:) - rho_sr(i,:); %#ok<*SAGROW>
     atransp = transpose(A);
     N = atransp*A;
+    % deltap are the deviations of computed from a priori coordinates. 
     deltap(:,i) = (N \ atransp) * transpose(deltay(i,:));
     % Compute the residuals.
     epsilon(:,i) = transpose(deltay(i,:)) - (A * deltap(:,i));
@@ -59,3 +60,39 @@ for i=1:length(epochs);
     sigmaz(:,i) = m0(:,i) * Q(3,3);
     sigmat(:,i) = m0(:,i) * Q(4,4);
 end
+%% Calculate station coordinates for each epoch.
+% Add or subtract the difference???
+wank_gps_coords = [wank_xr + deltap(1,:); wank_yr + deltap(2,:); ... 
+    wank_zr + deltap(3,:)]; 
+%% For ex 2.2: Plot satellite positions to perhaps explain why y accuracy 
+%% is better than x?
+% Drift may be because troposphere not yet corrected and not constant??
+
+%% B: 2.1 Single set of coordinates.
+% Simply average coordinates?
+wank_single_gps_coords = [mean(wank_gps_coords(1,:)); ...
+    mean(wank_gps_coords(2,:)); mean(wank_gps_coords(3,:))];
+% "Again give coordinate corrections and the formal errors."
+
+%% Calculate coordinates with tropospheric correction.
+% Calculate cos(z), the tropospheric zenith angle for each epoch.
+% Need a single set of station coordinates
+wank_xr_gps = wank_single_gps_coords(1,:);
+wank_yr_gps = wank_single_gps_coords(2,:);
+wank_zr_gps = wank_single_gps_coords(3,:);
+% OR use the a priori coordinates
+% wank_xr_gps = wank_xr
+xyz = sqrt((wank_xr_gps).^2 + (wank_yr_gps).^2 + (wank_zr_gps).^2);
+rho_sr = sqrt((wank_xs - wank_xr_gps).^2 ...
++ (wank_ys - wank_yr_gps).^2 + (wank_zs - wank_zr_gps).^2);
+cos_z = (wank_xr_gps./xyz) .* (wank_xs - wank_xr_gps)./rho_sr ...
+    + (wank_yr_gps./xyz) .* (wank_ys - wank_yr_gps)./rho_sr ...
+    + (wank_zr_gps./xyz) .* (wank_zs - wank_zr_gps)./rho_sr;
+% Tropospheric correction:
+tropo_corr = 2.3 ./ cos_z;
+% Apply correction.
+c1_corrected = wank_c1 + (c * wank_satt) + tropo_corr; % Or subtract???
+% Redo the coordinate calculation.
+% Call least-squares again as function (?)
+
+
