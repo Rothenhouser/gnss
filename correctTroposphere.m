1ecd2e0d58021adccaf23cd167b617651fb8c4cb
Rothenhouser <n.rothenhaeusler@gmail.com>
2014-11-28 16:24:18 +0100

    Start some refactoring.

---
+++ correctTroposphere.m
@@ -0,0 +1,20 @@
+function [corrected] = correctTroposphere(c1, satt, single_gps_coords, ...
+                                          xs, ys, zs, c)
+% Calculate cos(z), the tropospheric zenith angle for each epoch.
+% Need a single set of station coordinates
+xr_gps = single_gps_coords(1,:);
+yr_gps = single_gps_coords(2,:);
+zr_gps = single_gps_coords(3,:);
+% OR use the a priori coordinates
+% xr_gps = xr
+xyz = sqrt((xr_gps).^2 + (yr_gps).^2 + (zr_gps).^2);
+rho_sr = sqrt((xs - xr_gps).^2 ...
++ (ys - yr_gps).^2 + (zs - zr_gps).^2);
+cos_z = (xr_gps./xyz) .* (xs - xr_gps)./rho_sr ...
+    + (yr_gps./xyz) .* (ys - yr_gps)./rho_sr ...
+    + (zr_gps./xyz) .* (zs - zr_gps)./rho_sr;
+% Tropospheric correction:
+tropo_corr = 2.3 ./ cos_z;
+% Apply correction.
+corrected = c1 + (c * satt) + tropo_corr; % Or subtract???
+end
--- ex_2.m
+++ ex_2.m
@@ -7,6 +7,11 @@ dt = 30.0;
 ns = 7;
 % Earth rotation rate (rad/s)
 earth_rot_rate = 7.2921151467E-5;
+
+epochs = importdata('Epochs.txt');
+epochs = epochs(:,1);
+
+%% Station specific data.
 % A priori receiver coordinates (m)
 wank_xr = 4235956.688;
 wank_yr = 834342.467;
@@ -17,114 +22,128 @@ wank_satt = importdata('WANK_SATT');
 wank_xs_raw = importdata('WANK_SATX');
 wank_ys_raw = importdata('WANK_SATY');
 wank_zs_raw = importdata('WANK_SATZ');
-epochs = importdata('Epochs.txt');
-epochs = epochs(:,1);
+
+zugs_xr = 4246098.549;
+zugs_yr = 824269.097;
+zugs_zr = 4675790.018;
+% Recorded data
+zugs_c1 = importdata('ZUGS_C1');
+zugs_satt = importdata('ZUGS_SATT');
+zugs_xs_raw = importdata('ZUGS_SATX');
+zugs_ys_raw = importdata('ZUGS_SATY');
+zugs_zs_raw = importdata('ZUGS_SATZ');
+
+
+stn = 'zugs';
+if strcmp(stn, 'wank');
+    stn_xr = wank_xr;
+    stn_yr = wank_yr;
+    stn_zr = wank_zr;
+    stn_c1 = wank_c1;
+    stn_satt = wank_satt;
+    stn_xs_raw = wank_xs_raw;
+    stn_ys_raw = wank_ys_raw;
+    stn_zs_raw = wank_zs_raw;
+elseif strcmp(stn, 'zugs');
+    stn_xr = zugs_xr;
+    stn_yr = zugs_yr;
+    stn_zr = zugs_zr;
+    stn_c1 = zugs_c1;
+    stn_satt = zugs_satt;
+    stn_xs_raw = zugs_xs_raw;
+    stn_ys_raw = zugs_ys_raw;
+    stn_zs_raw = zugs_zs_raw;        
+end
+
 %% 3 (a): Correct satellite positions for Earth rotation during light
 %% travel time.
 % Raw geometric distance:
-rho_sr_raw = sqrt((wank_xs_raw - wank_xr).^2 ...
-+ (wank_ys_raw - wank_yr).^2 + (wank_zs_raw - wank_zr).^2);
+rho_sr_raw = sqrt((stn_xs_raw - stn_xr).^2 ...
++ (stn_ys_raw - stn_yr).^2 + (stn_zs_raw - stn_zr).^2);
 dOmega = rho_sr_raw * earth_rot_rate / c;
 % Correct the coordinates.
-wank_xs = wank_xs_raw .* cos(dOmega) + wank_ys_raw .* sin(dOmega);
-wank_ys = - wank_xs_raw .* sin(dOmega) + wank_ys_raw .* cos(dOmega);
-wank_zs = wank_zs_raw;
+stn_xs = stn_xs_raw .* cos(dOmega) + stn_ys_raw .* sin(dOmega);
+stn_ys = - stn_xs_raw .* sin(dOmega) + stn_ys_raw .* cos(dOmega);
+stn_zs = stn_zs_raw;
 % Recompute the geometric distance.
-rho_sr = sqrt((wank_xs - wank_xr).^2 ...
-+ (wank_ys - wank_yr).^2 + (wank_zs - wank_zr).^2);
+rho_sr = sqrt((stn_xs - stn_xr).^2 ...
++ (stn_ys - stn_yr).^2 + (stn_zs - stn_zr).^2);
 %% 3 (b) Compute the derivates of the observation equation.
 % Todo: change this back to c? Need to adjust calculations below!
 dPdt = 1;
 cs =[1 1 1 1 1 1 1] * dPdt;
-dPdx = -(wank_xs - wank_xr) ./ rho_sr;
-dPdy = -(wank_ys - wank_yr) ./ rho_sr;
-dPdz = -(wank_zs - wank_zr) ./ rho_sr;
+dPdx = -(stn_xs - stn_xr) ./ rho_sr;
+dPdy = -(stn_ys - stn_yr) ./ rho_sr;
+dPdz = -(stn_zs - stn_zr) ./ rho_sr;
 % Correct pseudorange data with satellite clock correction.
-c1_corrected = wank_c1 + (c * wank_satt); % changed sign before bracket
+c1_corrected = stn_c1 + (c * stn_satt); % changed sign before bracket
 %% Solve the normal equations.
 for i=1:length(epochs);
-    % Setup the grand design matrix A for every epoch.
-    A(:,1)=dPdx(i,:);
-    A(:,2)=dPdy(i,:);
-    A(:,3)=dPdz(i,:);
-    A(:,4)=cs;
-    % Solve normal equation.
-    deltay(i,:) = c1_corrected(i,:) - rho_sr(i,:); %#ok<*SAGROW>
-    atransp = transpose(A);
-    N = atransp*A;
-    % deltap are the deviations of computed from a priori coordinates. 
-    deltap(:,i) = (N \ atransp) * transpose(deltay(i,:));
-    % Compute the residuals.
-    epsilon(:,i) = transpose(deltay(i,:)) - (A * deltap(:,i));
-    m0(:,i) = sqrt(transpose(epsilon(:,i)) * epsilon(:,i) ./ 3);
-    % Formal errors
-    Q = N \ eye(size(N));  %changed, so singularity warning doesn't show 
-    sigmax(:,i) = m0(:,i) * Q(1,1);
-    sigmay(:,i) = m0(:,i) * Q(2,2);
-    sigmaz(:,i) = m0(:,i) * Q(3,3);
-    sigmat(:,i) = m0(:,i) * Q(4,4);
+% Setup the grand design matrix A for every epoch.
+A(:,1)=dPdx(i,:);
+A(:,2)=dPdy(i,:);
+A(:,3)=dPdz(i,:);
+A(:,4)=cs;
+% Solve normal equation.
+deltay(i,:) = c1_corrected(i,:) - rho_sr(i,:); %#ok<*SAGROW>
+atransp = transpose(A);
+N = atransp*A;
+% deltap are the deviations of computed from a priori coordinates.
+deltap(:,i) = (N \ atransp) * transpose(deltay(i,:));
+% Compute the residuals.
+epsilon(:,i) = transpose(deltay(i,:)) - (A * deltap(:,i));
+m0(:,i) = sqrt(transpose(epsilon(:,i)) * epsilon(:,i) ./ 3);
+% Formal errors
+Q = N \ eye(size(N)); %changed, so singularity warning doesn't show
+sigmax(:,i) = m0(:,i) * Q(1,1);
+sigmay(:,i) = m0(:,i) * Q(2,2);
+sigmaz(:,i) = m0(:,i) * Q(3,3);
+sigmat(:,i) = m0(:,i) * Q(4,4);
 end
 %% Calculate station coordinates for each epoch.
 % Add or subtract the difference???
-wank_gps_coords = [wank_xr + deltap(1,:); wank_yr + deltap(2,:); ... 
-    wank_zr + deltap(3,:)]; 
-%% For ex 2.2: Plot satellite positions to perhaps explain why y accuracy 
+stn_gps_coords = [stn_xr + deltap(1,:); stn_yr + deltap(2,:); ...
+stn_zr + deltap(3,:)];
+%% For ex 2.2: Plot satellite positions to perhaps explain why y accuracy
 %% is better than x?
 % Drift may be because troposphere not yet corrected and not constant??
 
 %% B: 2.1 Single set of coordinates.
 % Simply average coordinates?
-wank_single_gps_coords = [mean(wank_gps_coords(1,:)); ...
-    mean(wank_gps_coords(2,:)); mean(wank_gps_coords(3,:))];
+stn_single_gps_coords = [mean(stn_gps_coords(1,:)); ...
+mean(stn_gps_coords(2,:)); mean(stn_gps_coords(3,:))];
 % "Again give coordinate corrections and the formal errors."
 
 %% Calculate coordinates with tropospheric correction.
-% Calculate cos(z), the tropospheric zenith angle for each epoch.
-% Need a single set of station coordinates
-wank_xr_gps = wank_single_gps_coords(1,:);
-wank_yr_gps = wank_single_gps_coords(2,:);
-wank_zr_gps = wank_single_gps_coords(3,:);
-% OR use the a priori coordinates
-% wank_xr_gps = wank_xr
-xyz = sqrt((wank_xr_gps).^2 + (wank_yr_gps).^2 + (wank_zr_gps).^2);
-rho_sr = sqrt((wank_xs - wank_xr_gps).^2 ...
-+ (wank_ys - wank_yr_gps).^2 + (wank_zs - wank_zr_gps).^2);
-cos_z = (wank_xr_gps./xyz) .* (wank_xs - wank_xr_gps)./rho_sr ...
-    + (wank_yr_gps./xyz) .* (wank_ys - wank_yr_gps)./rho_sr ...
-    + (wank_zr_gps./xyz) .* (wank_zs - wank_zr_gps)./rho_sr;
-% Tropospheric correction:
-tropo_corr = 2.3 ./ cos_z;
-% Apply correction.
-c1_corr_tropo = wank_c1 + (c * wank_satt) + tropo_corr; %addition of tropo_corr is correct!
+c1_corr_tropo = correctTroposphere(stn_c1, stn_satt, ...
+    stn_single_gps_coords, stn_xs, stn_ys, stn_zs, c);
 % Redo the coordinate calculation.
 % Call least-squares again as function (?)
-
 % Part B, 2.3
 % All the corrections need to be applied to the pseudorange observations
 % before the LS-alogrithm is executed???
+
 % Compute satellite inertial velocities:
 delta_epoch = 30.0;
 for i = 1:length(epochs)-1;
-    % uses 2point finite differences -> no derivative for last epoch!    
-    inert_vel_x(i,:) = (wank_xs(i+1,:) - wank_xs(i,:)) / dt;
-    inert_vel_y(i,:) = (wank_ys(i+1,:) - wank_ys(i,:)) / dt;
-    inert_vel_z(i,:) = (wank_zs(i+1,:) - wank_zs(i,:)) / dt;
-    % Correct for earth's rotation
-    inert_vel_x_corrected(i,:) = inert_vel_x(i,:) - earth_rot_rate .* wank_ys(i,:);
-    inert_vel_y_corrected(i,:) = inert_vel_y(i,:) + earth_rot_rate .* wank_xs(i,:);
-    inert_vel_z_corrected(i,:) = inert_vel_z(i,:);
+% uses 2point finite differences -> no derivative for last epoch!
+inert_vel_x(i,:) = (stn_xs(i+1,:) - stn_xs(i,:)) / dt;
+inert_vel_y(i,:) = (stn_ys(i+1,:) - stn_ys(i,:)) / dt;
+inert_vel_z(i,:) = (stn_zs(i+1,:) - stn_zs(i,:)) / dt;
+% Correct for earth's rotation
+inert_vel_x_corrected(i,:) = inert_vel_x(i,:) - earth_rot_rate .* stn_ys(i,:);
+inert_vel_y_corrected(i,:) = inert_vel_y(i,:) + earth_rot_rate .* stn_xs(i,:);
+inert_vel_z_corrected(i,:) = inert_vel_z(i,:);
 end
-
 % Discard last epoch from sattelite postions to have same matrix dimensions
-wank_xs_short = wank_xs(1:end-1,:);
-wank_ys_short = wank_ys(1:end-1,:);
-wank_zs_short = wank_zs(1:end-1,:);
-
+stn_xs_short = stn_xs(1:end-1,:);
+stn_ys_short = stn_ys(1:end-1,:);
+stn_zs_short = stn_zs(1:end-1,:);
 % Compute relativistic correction according to given formula
 for i = 1:ns;
-    delta_tk(:,i) = -2 * (wank_xs_short(:,i) .* inert_vel_x_corrected(:,i) ...
-        + wank_ys_short(:,i) .* inert_vel_y_corrected(:,i) ...
-        + wank_zs_short(:,i) .* inert_vel_z_corrected(:,i)) / c^2;
-    % relativistic correction in meters
-    delta_tk_m(:,i) = delta_tk(:,i) .* c;
+delta_tk(:,i) = -2 * (stn_xs_short(:,i) .* inert_vel_x_corrected(:,i) ...
++ stn_ys_short(:,i) .* inert_vel_y_corrected(:,i) ...
++ stn_zs_short(:,i) .* inert_vel_z_corrected(:,i)) / c^2;
+% relativistic correction in meters
+delta_tk_m(:,i) = delta_tk(:,i) .* c;
 end
